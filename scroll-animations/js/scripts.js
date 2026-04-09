b2e7e322d8c840bdc254e7090c37cd0b686f4b41
Juan Gomez <Juancarlosgomez0259@gmail.com>
2026-04-08 20:23:46 -0600

    added javascript animation

---
+++ scroll-animations/js/scripts.js
@@ -0,0 +1,23 @@
+
+//keep functions on top
+const h2 = document.querySelectorAll('h2');
+const hiddenElements = document.querySelectorAll('.hidden');
+const observer = new IntersectionObserver((entry) => {
+    // creates a forEach loop to loop through the entries
+    entry.forEach((entry) => {
+
+        if (entry.isIntersecting) {
+            // using replace instead of add and remove to prevent class list from growing infinitely
+            entry.target.classList.replace('hidden', 'show');
+        } else {
+
+            entry.target.classList.replace('show', 'hidden');
+        }
+
+    });
+// set threshold to 0.3 so that the animation will trigger when the element is 30% visible in the viewport
+}, { threshold: 0.3 });
+
+// observe all elements with the class "hidden" and "h2"
+hiddenElements.forEach((el) => observer.observe(el));
+h2.forEach((el) => observer.observe(el));
